592bcfecf1465666419e15bb8664aa04f410e462
Fabricio <fabricio.pacheco@fisagrp.com>
2025-10-27 15:25:23 -0500

    ps updated

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def hello_world():
-    return 'Hola Mundo', 200
+def call_app_b():
+    try:
+        # Llamamos a la app B, que está en localhost:3001
+        response = requests.get('http://route-occasional-iguana-ffisa-dev.apps.rm1.0a51.p1.openshiftapps.com/devCurso:4000/')
+        return f'Respuesta de app B: {response.text}', response.status_code
+    except requests.exceptions.ConnectionError:
+        return 'Error: no se pudo conectar con app B', 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
